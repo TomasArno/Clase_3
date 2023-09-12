0f37a3fe795d2b7283537194f2a3357d5bf6f52f
TomasArno <arnotomas1@gmail.com>
2023-09-12 15:23:01 -0300

    Add project files.

---
+++ ClassLibraryStudent/StudentClass.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ClassLibraryStudent
+{
+    public class Student
+    {
+        static int random;
+
+        public string nombre;
+        public string apellido;
+        public int legajo;
+
+        float _notaPrimerParcial;
+        float _notaSegundoParcial;
+        static Student()
+        {
+            Random randomNum = new Random();
+            random = randomNum.Next(6,10);
+        }
+
+        public Student(string nombre, string apellido, int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public void SetNotaPrimerParcial(float notaPrimerParcial)
+        {
+            this._notaPrimerParcial = notaPrimerParcial;
+        }
+
+        public void SetNotaSegundoParcial(float notaSegundoParcial)
+        {
+            this._notaSegundoParcial = notaSegundoParcial;
+        }
+
+        float CalcularPromedio()
+        {
+            return (this._notaPrimerParcial + this._notaSegundoParcial) / 2;
+        }
+
+        public dynamic CalcularNotaFinal()
+        {
+            if (this._notaPrimerParcial >= 4 && this._notaSegundoParcial >= 4)
+            {
+                return random;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public void Mostrar()
+        {
+            
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Nombre completo: {this.nombre} {this.apellido} \nNro. legajo {this.legajo} \n");
+            sb.Append($"Nota primer parcial: {this._notaPrimerParcial} \nNota segundo parcial: {this._notaSegundoParcial} \n");
+            sb.Append($"Promedio: {this.CalcularPromedio()} \n");
+
+            float notaFinal = this.CalcularNotaFinal();
+
+            if (notaFinal != -1)
+            {
+                sb.Append($"NOTA FINAL: {notaFinal}\n\n");
+            }
+            else
+            {
+                sb.Append($"NOTA FINAL: Alumno Desaprobado \n\n");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibraryStudent;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student estudiante1 = new Student("Tomás", "Arno", 2244);
+            Student estudiante2 = new Student("Matías", "Arnoski", 2444);
+            Student estudiante3 = new Student("mia", "Arneitor", 2222);
+
+            estudiante1.SetNotaPrimerParcial(10);
+            estudiante1.SetNotaSegundoParcial(5);
+            estudiante1.Mostrar();
+
+            estudiante2.SetNotaPrimerParcial(4);
+            estudiante2.SetNotaSegundoParcial(4);
+            estudiante2.Mostrar();
+
+            estudiante3.SetNotaPrimerParcial(4);
+            estudiante3.SetNotaSegundoParcial(3);
+            estudiante3.Mostrar();
+        }
+    }
+}
